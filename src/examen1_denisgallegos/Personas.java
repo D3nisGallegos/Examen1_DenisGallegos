2c626286f02d61ee8242fa2853be5dea0e4faeb0
Denis Jose <denisj.gallegos@unitec.edu>
2020-11-13 13:35:06 -0600

    Primer commit

---
+++ src/examen1_denisgallegos/Personas.java
@@ -0,0 +1,65 @@
+/*Clase para elobjeto PERSONAS
+ */
+package examen1_denisgallegos;
+
+
+public class Personas {
+    
+    private String nombre; 
+    private int edad; 
+    private String sexo; 
+    private Arma arma; 
+
+    public Personas() {
+    }
+
+    public Personas(String nombre, int edad, String sexo, Arma arma) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.arma = arma;
+    }
+    
+    //MUTADORES: 
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public Arma getArma() {
+        return arma;
+    }
+
+    public void setArma(Arma arma) {
+        this.arma = arma;
+    }
+    
+    //MA: 
+    public String toStringPersonas(){
+        
+    }
+    
+    
+     
+    
+}//Fin de la clase.
